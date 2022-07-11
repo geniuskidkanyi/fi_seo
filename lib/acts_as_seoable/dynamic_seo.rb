e00c4a321a75b66b6fa543d878f9887f3ed14de3
Muhammed Kanyi <kanyi@gcubed.gm>
2022-07-11 19:40:42 +0000

    fixed validation

--- lib/acts_as_seoable/dynamic_seo.rb
+++ lib/acts_as_seoable/dynamic_seo.rb
@@ -1,6 +1,6 @@
 class DynamicSeo < ActiveRecord::Base
 
   belongs_to :seoable, polymorphic: true, inverse_of: :seoable
-  validates_presence_of   :seoable_type
+  # validates_presence_of   :seoable_type
   # validates_uniqueness_of :seoable_id, scope: :seoable_type
 end
