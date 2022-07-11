5b19f05c05f158670b703881483d2e55abb137aa
Muhammed Kanyi <kanyi@gcubed.gm>
2022-07-11 18:18:54 +0000

    added inverse of

--- lib/acts_as_seoable/dynamic_seo.rb
+++ lib/acts_as_seoable/dynamic_seo.rb
@@ -1,6 +1,6 @@
 class DynamicSeo < ActiveRecord::Base
 
-  belongs_to :seoable, polymorphic: true
+  belongs_to :seoable, polymorphic: true, inverse_of: :seoable
   validates_presence_of   :seoable_type, :seoable_id
   validates_uniqueness_of :seoable_id, scope: :seoable_type
 end
