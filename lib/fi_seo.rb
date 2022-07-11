9db14549296876cb50edb110683e7232d4ea8131
Muhammed Kanyi <kanyi@gcubed.gm>
2022-07-11 19:45:39 +0000

    fix duplicate

--- lib/fi_seo.rb
+++ lib/fi_seo.rb
@@ -148,8 +148,8 @@ module FiSeo
 
 
     def create_dynamic_seo_record
-       DynamicSeo.create(seoable_type: self.class.to_s, seoable_id: id, title: self.title_value,
-                        description: self.description_value, keywords: self.keywords_value)
+       DynamicSeo.create_with(title: self.title_value,
+                        description: self.description_value, keywords: self.keywords_value).find_or_create_by(seoable_type: self.class.to_s, seoable_id: self.id)
     end
 
     def update_dynamic_seo_record
